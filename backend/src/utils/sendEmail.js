2e642dd0b1f6fef59e67e76aa26d9e8aaddc5f66
dopi95 <elyasyenealem@gmail.com>
2025-10-13 10:35:08 +0300

    fix: Replace notification template variables with actual student data

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -22,7 +22,7 @@ const sendEmail = async (options) => {
           secure: false,
           auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.BREVO_API_KEY,
+            pass: process.env.EMAIL_PASS,
           },
           tls: {
             rejectUnauthorized: false
